934b4d3d2c1b8baca0f43a457d6a8dc9cb5371be
Sari <tn24yv@brocku.ca>
2026-01-12 23:52:14 -0500

    updated description in the hero section

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ import { Code, BookOpen, Award, Users, Monitor, Terminal, Database, Cpu, Gamepad
 
 export const userData = {
     name: "Sarisha Kadakia", // Updated name
-    logline: "A CS major in Brock University building the future of spatial experiences through code and art.",
+    logline: "Aspiring Software Engineer | CS Major @ Brock University | Passionate about Full-Stack Development & System Architecture ",
     heroImage: "/hero-bg.jpg", // We need to handle the uploaded image. I'll assume it's placed in public/
 };
 
