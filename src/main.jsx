0905aab30d8aeca940c79e3c25afa15de121a720
Sari <tn24yv@brocku.ca>
2026-01-21 02:57:08 -0500

    chore: Force redeploy to clear cache and apply fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Deployed check: 2026-01-21 T 02:50
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
